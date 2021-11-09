9fe2bebb5e9f3884db1d033c7bea98f9a40cb7b6
hschielzeth <hschielzeth@users.noreply.github.com>
2021-11-09 10:26:44 +0100

    Add files via upload
    Various smaller changes, including the addition of covxmin and covxmax to allests and resEst (to track the range of the covariate)

--- RandSlopeR2_Simulation.R
+++ RandSlopeR2_Simulation.R
@@ -2,12 +2,27 @@
 # Simulation
 ####################################################################################################################
 
-rm(list=ls())
-require(lme4)
-require(psych)
+# R version 4.1.2 (2021-11-01)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Matrix products: default
+# attached base packages:
+# stats     graphics  grDevices utils     datasets  methods   base     
+# lme4_1.1-27.1    Matrix_1.3-4     matrixcalc_1.0-5 psych_2.1.9     
+# loaded via a namespace (and not attached):
+# Rcpp_1.0.7      lattice_0.20-45 MASS_7.3-54     grid_4.1.2      nlme_3.1-153    minqa_1.2.4    
+# nloptr_1.2.2.2  boot_1.3-28     splines_4.1.2   tools_4.1.2     parallel_4.1.2  compiler_4.1.2 
+# mnormt_2.0.2    tmvnsim_1.0-2  
 
-source("ADDPATH\\RandSlopeR2_PlottingFunctions.R")
-source("ADDPATH\\RandSlopeR2_SimulationFunctions.R")
+rm(list=ls())
+require(psych)
+require(matrixcalc)
+require(lme4)
+sessionInfo()
+
+path = "C:\\Users\\Holger\\Documents\\A_PaperDrafts\\B_Submitted\\HS_RandomSlopeR2\\Submitted\\Submitted to MethEcolEvol\\"
+
+source(paste0(path, "RandSlopeR2_SimulationFunctions.R"))
+source(paste0(path, "RandSlopeR2_PlottingFunctions.R"))
 
 scenario = 0
 #scenario = 1 # Random slopes not fitted
@@ -61,18 +76,19 @@ if(scenario == 6) { # lognormal x distribution
 Sigma = matrix(c(Vu, Cuv, Cuv, Vv), ncol=2)
 if(!matrixcalc::is.positive.definite(Sigma)) cat("Sigma is not positive definite!\n")
 nobs     = nind*nrep
-settings = c(nind=nind, nrep=nrep, alpha=alpha, betaX=betaX, betaX2=betaX2, meanX=meanX, meanX2=meanX2, Vx=Vx, Vx2=Vx2, Vr=Vr, Vu=Vu, Vv=Vv, Cuv=Cuv, randSlopeOmitted=randSlopeOmitted, errX=errX, balanced=balanced, xdist=xdist)
+settings = c(nind=nind, nrep=nrep, alpha=alpha, betaX=betaX, betaX2=betaX2, meanX=meanX, meanX2=meanX2, Vx=Vx, Vx2=Vx2, Vr=Vr, Vu=Vu, Vv=Vv, Cuv=Cuv, 
+			 randSlopeOmitted=randSlopeOmitted, errX=errX, balanced=balanced, xdist=xdist)
 
 # Sim calculations
 condRsim    = varDecomp(vect=settings)
-allsim      = c(settings, condRsim)
+allsim      = c(settings, condRsim, covxmin=NA, covxmax=NA)
 
 # Simulation, model fittings and extraction
 md        = simData(vect=settings)
 mod       = modFit(md, randSlopeOmitted)
 ests      = estExtr(mod, md, randSlopeOmitted)
 condRests = varDecomp(vect=ests)
-allests   = c(ests, condRests)
+allests   = c(ests, condRests, covxmin=min(md$x), covxmax=max(md$x))
 
 # Visual check for whether condition R is a good fit to 
 # between-individual variance estimated for bins of x
@@ -80,14 +96,14 @@ windows()
 plotCheck(md, settings, ests, binfact=3, ptscale=5, las=1)
 
 # Preparation for looping
-nsim=200
+nsim=500
 resSim = allsim
 resEst = allests
 for(i in 2:nsim) resEst = rbind(resEst, rep(NA, length(allests))) 
 rownames(resEst) = NULL
 
 # Setup of pregressive plot for simulation
-finalOrd = rev(c("alpha", "betaX", "meanX", "Vx", "betaX2", "meanX2", "Vx2",  
+finalOrd = rev(c("alpha", "betaX", "meanX", "Vx", "betaX2", "meanX2", "Vx2", 
 	"Vu", "Vv", "Cuv", "Vp", "Vr", "Vo", "Vf", "Vi", "Vs", "Rmar", "R2s", "xmin", "minVix"))
 ylabs = rev(c(expression(alpha), expression(beta[X]), expression(mu[X]), expression("V"[X]), 
 		expression(beta[X2]), expression(mu[X2]), expression("V"[X2]), 
@@ -108,15 +124,15 @@ for(i in 2:nsim) {
 	mod = modFit(md, randSlopeOmitted)
 	ests = estExtr(mod, md, randSlopeOmitted)
 	condRests = varDecomp(vect=ests)
-	allests = c(ests, condRests)
+	allests = c(ests, condRests, covxmin=min(md$x), covxmax=max(md$x))
 	resEst[i,] = allests
 	points((allests[finalOrd]-as.numeric(allsim[finalOrd]))/abs(as.numeric(allsim[finalOrd])), jitter(1:length(finalOrd)))
 }
 
-# Plot for Figure 2
+# Figure 3
 windows()
 par(mfrow=c(1,1), mar=c(5,5,1.4,4))
-plot((allests[finalOrd]-as.numeric(allsim[finalOrd]))/abs(as.numeric(allsim[finalOrd])), jitter(1:length(finalOrd)), ylab="", type="n", yaxt="n", ylim=c(1,length(finalOrd)), xlab="", xlim=c(-1, 1))
+plot((allests[finalOrd]-as.numeric(allsim[finalOrd]))/abs(as.numeric(allsim[finalOrd])), jitter(1:length(finalOrd)), ylab="", type="n", yaxt="n", ylim=c(1,length(finalOrd)), xlab="", xlim=c(-1.1, 1.4))
 title(xlab="Proportional difference to simulated values\n(Est-Sim)/Sim", line=3.5)
 axis(2, at=1:length(finalOrd), labels=ylabs, las=1)
 axis(4, at=1:length(finalOrd), labels=paste0("= ", round(as.numeric(allsim[finalOrd]),2)), las=1, tick=FALSE, line=-0.3)
@@ -133,7 +149,8 @@ for(i in 1:length(finalOrd)) {
 	segments(x0=mean(devs), x1=mean(devs), y0=i-0.5, y1=i+0.5, col="red", lwd=3)
 }
 
-# Figure 3-4 (depending on scenarios)
+# Figure 2 and 4 (depending on scenarios)
 windows(7,7)
 par(mfrow=c(1,1))
-plotFig2c(resEst, allsim, main=paste0("Scenario ", scenario))
+source(paste0(path, "RandSlopeR2_PlottingFunctions.R"))
+plotFig2(resEst, allsim, main=paste0("Scenario ", scenario))
